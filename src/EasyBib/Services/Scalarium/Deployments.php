d1f6d9d76bf42cd8fe338be56705cdef10cc2ee4
Ulf Harnhammar <ulfharn@gmail.com>
2012-08-01 14:28:02 +0200

    no deployments of clouds

--- src/EasyBib/Services/Scalarium/Deployments.php
+++ src/EasyBib/Services/Scalarium/Deployments.php
@@ -9,19 +9,12 @@ class Deployments extends Scalarium
 {
     function getDeployments()
     {
-        $deployments = array('applications' => array(), 'clouds' => array());
+        $deployments = array();
         $applications = new Applications($this->_endpoint, $this->_accept, $this->_token);
         $applicationsData = $applications->getApplications();
         foreach ($applicationsData as $oneApplication) {
-            $deployments['applications'][ $oneApplication['id'] ] = $applications->getDeploymentsByApplication($oneApplication['id']);
+            $deployments[ $oneApplication['id'] ] = $applications->getDeploymentsByApplication($oneApplication['id']);
         }
-/*
-        $clouds = new Clouds($this->_endpoint, $this->_accept, $this->_token);
-        $cloudsData = $clouds->getClouds();
-        foreach ($cloudsData as $oneCloud) {
-            $deployments['clouds'][ $oneCloud['id'] ] = $clouds->getDeploymentsByCloud($oneCloud['id']);
-        }
-*/
         return $deployments;
     }
 }
